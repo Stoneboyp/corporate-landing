f80de78ef540d8e9f01b4b83edf27da34d8301ad
StoneboyP <melnikovwor@gmail.com>
2025-04-28 20:50:33 +0500

    ref:contact section

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,5 +1,5 @@
 import { Paper } from "@mui/material";
-import Map from "../Map/Map";
+// import Map from "../Map/Map";
 import { useTranslation } from "react-i18next";
 
 const ContactSection = () => {
@@ -10,15 +10,7 @@ const ContactSection = () => {
       <section className="contact-section">
         <h1>{t("contacts.title")}</h1>
         <p>
-          {t("contacts.address")}{" "}
-          <a
-            href="https://go.2gis.com/xSBkb"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ textDecoration: "none", color: "#004b87" }}
-          >
-            {t("contacts.addressDetail")}
-          </a>
+          {t("contacts.address")} <a>{t("contacts.addressDetail")}</a>
         </p>
         <p>
           {t("contacts.phone")}: <span>+7 777 210 4820</span>
@@ -27,7 +19,7 @@ const ContactSection = () => {
           {t("contacts.email")}: <span>info@caspianconsult.kz</span>
         </p>
       </section>
-      <Map />
+      {/* <Map /> */}
     </Paper>
   );
 };
